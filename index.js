0d0805a0b298acec6c15bb7e6e5a11e34dc4f83b
Tommie Jakobsson <tommie@stambokaj.se>
2020-10-21 18:03:21 +0200

    Added node/express server + nodemon

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const { loadNuxt, build } = require('nuxt');
+const PORT = process.env.PORT || 3000;
+const dev = process.env.NODE_ENV !== "production";
+
+(async () => {
+  try {
+    const nuxt = await loadNuxt({
+      for: dev ? 'dev' : 'start'
+    });
+    
+    const app = express();
+
+    app.use(nuxt.render);
+    if (dev) build(nuxt);
+
+    app.listen(PORT, err => {
+      if (err) throw err;
+      console.log(`ready at http://localhost:${PORT}`);
+    });
+  } catch (e) {
+    console.error(e);
+    process.exit(1);
+  }
+})();
